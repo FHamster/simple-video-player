e50488dfc8703c2e5c6ee51cdcea20dff3967234
py-diviner <1597457191@qq.com>
2019-08-04 10:02:54 +0800

    2

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+var bodyParser = require('body-parser')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index')
+var apiRouter = require('./routes/api')
+var userRouter = require('./routes/userRouter')
+var markRouter = require('./routes/markRouter')
+var musicMarkRouter = require('./routes/musicMarkRouter')
+var musicRouter = require('./routes/musicRouter')
+var videoRouter = require('./routes/videoRouter')
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// parse application/json
+app.use(bodyParser.json())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-module.exports = app;
+//路由列表
+app.use('/', indexRouter)
+app.use('/api', apiRouter)
+app.use('/api', userRouter)
+app.use('/api', markRouter)
+app.use('/api', musicMarkRouter)
+app.use('/api', musicRouter)
+app.use('/api', videoRouter)
+
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+let MyQuery = require('./MySqlInter')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+router.get('/getTest', function (req, res) {
+  res.json({ state: 'GetOK' })
+})
+router.post('/postTest', (req, res) => {
+  res.json({ state: 'PostOK' })
+})
+
+router.delete('/deleteTest', (req, res) => {
+  res.json({ state: 'DeleteOK' })
+})
+
+router.put('/putTest', (req, res) => {
+  res.json({ state: 'PutOK' })
+})
+
+router.get('/getIDTest/:id', function (req, res) {
+  console.log(req.params.id)
+  res.json({ state: 'url动态参数测试通过', value: req.params.id })
+})
+
+router.get('/sqlTest', (req, res) => {
+  MyQuery('select * from test1', (data) => {
+    console.log(data)
+    res.json(data)
+  })
+})
+module.exports = router
